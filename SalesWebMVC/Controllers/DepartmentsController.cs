d2b7baee2cb43bc7692d073deeea6d600b81dd8d
Julio Barros <julio11lp@hotmail.com>
2022-04-07 17:16:41 -0300

    feat: created Departments controller and view

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+  public class DepartmentsController : Controller
+  {
+    public IActionResult Index()
+    {
+      List<Department> departments = new List<Department>();
+      departments.Add(new Department(1, "Eletronics"));
+      departments.Add(new Department(2, "Fashion"));
+
+      return View(departments);
+    }
+  }
+}
