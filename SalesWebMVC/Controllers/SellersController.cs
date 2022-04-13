27835fb058b163cb4b8c0ab05ea92d989dd99c86
Julio Barros <julio11lp@hotmail.com>
2022-04-13 11:45:11 -0300

    feat: created the delete seller feature

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -36,5 +36,24 @@ namespace SalesWebMVC.Controllers
       _sellerService.Insert(seller);
       return RedirectToAction(nameof(Index));
     }
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+      if (id == null) return NotFound();
+
+      var seller = _sellerService.FindById(id.Value);
+
+      if (seller == null) return NotFound();
+
+      return View(seller);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id)
+    {
+      _sellerService.Remove(id);
+      return RedirectToAction(nameof(Index));
+    }
   }
 }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -21,6 +21,18 @@ namespace SalesWebMVC.Services
         .ToList();
     }
 
+    public Seller FindById(int id)
+    {
+      return _context.Seller.Include(x => x.Department).FirstOrDefault(x => x.Id == id);
+    }
+
+    public void Remove(int id)
+    {
+      var seller = FindById(id);
+      _context.Seller.Remove(seller);
+      _context.SaveChanges();
+    }
+
     public void Insert(Seller seller)
     {
       _context.Add(seller);
